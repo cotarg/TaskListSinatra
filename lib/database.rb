b4963f715e1d0c8362a5787a33bf8bb849413ea9
Valerie <val.conklin@gmail.com>
2016-04-12 13:29:12 -0700

    Made create_schema and reset_schema methods

--- lib/database.rb
+++ lib/database.rb
@@ -2,6 +2,7 @@ require "sqlite3"
 
 module TaskList
   class Database
+    attr_reader :db
 
     def initialize(db_name)
       @db = SQLite3::Database.new(db_name)
@@ -10,6 +11,23 @@ module TaskList
     def create_schema
       # Put your ruby code here to use the @db variable
       # to setup your schema in the database.
+
+      query = <<-CREATESTATEMENT
+        CREATE TABLE tasks (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT,
+        completed_at TEXT
+        );
+      CREATESTATEMENT
+
+      db.execute(query)
     end
+
+    def reset_schema!
+      db.execute("DROP TABLE IF EXISTS tasks;")
+      create_schema
+    end
+    
   end
 end
