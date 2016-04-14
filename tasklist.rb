29761cbef285bcf4f19925f1fcbf0c6e10d8bbc8
Valerie <val.conklin@gmail.com>
2016-04-14 15:43:44 -0700

    Changed submit button so it's now just a regular link, and used query strings to populate edit page's default values for each task YAY

--- tasklist.rb
+++ tasklist.rb
@@ -18,19 +18,22 @@ class TaskListApp < Sinatra::Base
   end
 
   post '/add' do
-    @task = TaskList::Task.new
-    @task.add_task(params["title"], params["description"], params["completed_at"])
+    @task_manager = TaskList::Task.new
+    @task_manager.add_task(params["title"], params["description"], params["completed_at"])
     @displayed_task = TaskList::Task.new.all_tasks
     redirect '/'
   end
 
-  post '/edit' do
-    erb :edit
-  end
+  # post '/edit' do
+  #   @task_manager = TaskList::Task.new
+  #   @retrieved_task = @task_manager.find_task(params["task_id"]).first
+  #   erb :edit
+  # end
 
 
   get '/edit' do
-
+    @task_manager = TaskList::Task.new
+    @retrieved_task = @task_manager.find_task(params["task_id"]).first
     erb :edit
   end
 
